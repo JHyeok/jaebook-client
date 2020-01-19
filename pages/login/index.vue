a633742a0ea0ee9bb250e5307dfb29d5d8dfb307
JHyeok <dev.jhyeok@gmail.com>
2020-01-19 11:39:12 +0900

    feat: 로그인 페이지 추가

---
+++ pages/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container page-container">
+    <div>
+      <login />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import Login from '@/components/Login.vue'
+
+@Component({
+  components: {
+    Login
+  }
+})
+export default class LoginPage extends Vue {
+}
+</script>
