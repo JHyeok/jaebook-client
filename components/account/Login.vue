1a996ff02b6322ba0e592dba99db0f77f5a3afba
JHyeok <dev.jhyeok@gmail.com>
2020-01-22 18:28:59 +0900

    fix: 로그인 페이지 디자인 수정

--- components/account/Login.vue
+++ components/account/Login.vue
@@ -1,23 +1,32 @@
 <template>
-  <form @submit.prevent="login">
-    <p class="h4 text-center mb-4">
-      JaeBook
-    </p>
-    <div class="form-group">
-      <label for="email" class="grey-text">이메일</label>
-      <input id="email" v-model.trim="email" type="email" placeholder="이메일" class="form-control">
-    </div>
-    <br>
-    <div class="form-group">
-      <label for="password" class="grey-text">비밀번호</label>
-      <input id="password" v-model.trim="password" type="password" placeholder="비밀번호" class="form-control">
-    </div>
-    <div class="text-center mt-4">
-      <button class="btn btn-success" :disabled="!isLogin" type="submit">
+  <div class="row">
+    <div class="col-md-6 offset-md-3 col-xs-12">
+      <h1 class="text-center">
         로그인
-      </button>
+      </h1>
+      <p class="text-center">
+        <nuxt-link :to="`/account/register`">
+          계정이 필요하신가요?
+        </nuxt-link>
+      </p>
+      <form @submit.prevent="login">
+        <p class="h4 text-center mb-4">
+          JaeBook
+        </p>
+        <div class="form-group">
+          <input id="email" v-model.trim="email" type="email" placeholder="이메일" class="form-control form-control-lg">
+        </div>
+        <div class="form-group">
+          <input id="password" v-model.trim="password" type="password" placeholder="비밀번호" class="form-control form-control-lg">
+        </div>
+        <div class="text-right mt-4">
+          <button class="btn btn-success btn-lg" :disabled="!isLogin" type="submit">
+            로그인
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
+  </div>
 </template>
 
 <script lang="ts">
--- pages/account/login.vue
+++ pages/account/login.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="container page-container">
-    <div>
-      <login />
-    </div>
+    <login />
   </div>
 </template>
 
