88009e57590876ed969133e45cf03488565cf77a
JHyeok <dev.jhyeok@gmail.com>
2020-01-23 12:23:32 +0900

    fix: Header 디자인 수정

--- components/home/HeaderNav.vue
+++ components/home/HeaderNav.vue
@@ -1,46 +1,25 @@
 <template>
-  <header>
-    <nav
-      class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3 fixed-top"
-    >
-      <div class="container">
-        <nuxt-link
-          :to="`/`"
-          class="navbar-brand"
-        >
-          JAEBOOK
-        </nuxt-link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target=".navbar-collapse"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon" />
-        </button>
-        <div
-          class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse"
-        >
-          <ul class="navbar-nav flex-grow-1">
-            <li class="nav-item">
-              <nuxt-link :to="`/account/login`" class="nav-link text-dark">
-                로그인
-              </nuxt-link>
-            </li>
-            <li class="nav-item">
-              <nuxt-link
-                :to="`/account/register`"
-                class="nav-link text-dark"
-              >
-                회원가입
-              </nuxt-link>
-            </li>
-          </ul>
-        </div>
-      </div>
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight-normal">
+      <nuxt-link :to="`/`" class="p-2 text-dark font-weight-normal">
+        JAEBOOK
+      </nuxt-link>
+    </h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+      <nuxt-link :to="`/account/login`" class="p-2 text-dark">
+        로그인
+      </nuxt-link>
+      <nuxt-link :to="`/account/register`" class="p-2 text-dark">
+        회원가입
+      </nuxt-link>
     </nav>
-  </header>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class HeaderNavComponent extends Vue {
+}
+</script>
