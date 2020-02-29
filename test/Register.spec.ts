79d005e7bcc04b22531e93268896bda640c7df83
JHyeok <dev.jhyeok@gmail.com>
2020-02-29 12:32:39 +0900

    chore: 회원가입 Component 삭제

--- test/Register.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Register from '@/components/account/Register.vue'
-
-describe('Register', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Register)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
