993be7e595f638ea751f2c5e996c7184ffd0de5e
JHyeok <dev.jhyeok@gmail.com>
2020-02-29 11:20:44 +0900

    test: 사용하지 않는 단위 테스트 삭제

--- test/HeaderNav.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import HeaderNav from '@/components/home/HeaderNav.vue'
-
-describe('HeaderNav', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(HeaderNav)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
--- test/Login.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Login from '@/components/account/Login.vue'
-
-describe('Login', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Login)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
