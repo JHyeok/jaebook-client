2833c957afca449e97981619d84f56cec937291d
JHyeok <dev.jhyeok@gmail.com>
2020-03-04 22:51:57 +0900

    feat: 게시글 무한 스크롤 기능 추가

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -16,6 +16,16 @@
           <post-card :on-view="viewPost" :post="post" />
         </div>
       </template>
+      <infinite-loading spinner="spiral" @infinite="infiniteHandler">
+        <div slot="no-more" />
+        <div slot="no-results">
+          No Posts
+        </div>
+        <div slot="error" slot-scope="{ trigger }">
+          Error message, click
+          <a href="javascript:;" @click="trigger">here</a> to retry
+        </div>
+      </infinite-loading>
     </div>
   </main>
 </template>
@@ -23,20 +33,25 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import InfiniteLoading from 'vue-infinite-loading'
 import PostCard from '~/components/post/PostCard.vue'
 Component.registerHooks(['asyncData'])
 
 @Component({
   components: {
-    PostCard
+    PostCard,
+    InfiniteLoading
   }
 })
 export default class PostsPage extends Vue {
-  private posts = []
+  private posts: any = []
+  private page: number = 1
+  private offset: number = 0
+  private limit: number = 20
 
   async asyncData (context) {
     try {
-      const data = await context.$axios.$get('/posts')
+      const data = await context.$axios.$get('/posts?offset=0&limit=20')
       return {
         posts: data
       }
@@ -44,6 +59,20 @@ export default class PostsPage extends Vue {
       // eslint-disable-next-line no-console
       console.log(error)
     }
+  }
+
+  infiniteHandler ($state) {
+    setTimeout(async () => {
+      this.page++
+      this.offset = (this.page - 1) * this.limit
+      const data = await (this as any).$axios.$get(`/posts?offset=${this.offset}&limit=${this.limit}`)
+      if (data.length > 0) {
+        this.posts = [...this.posts, ...data]
+        $state.loaded()
+      } else {
+        $state.complete()
+      }
+    }, 0)
   }
 
   viewPost (postId): void {
