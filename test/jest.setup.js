ed94d67cc2506cc2616ba67ab48c515b0baebbca
JHyeok <dev.jhyeok@gmail.com>
2020-03-06 22:10:48 +0900

    feat: Jest TypeScript 환경 설정

--- test/jest.setup.js
+++
@@ -1,5 +0,0 @@
-import VueTestUtils from '@vue/test-utils'
-
-// Mock Nuxt Components
-VueTestUtils.config.stubs['nuxt-link'] = '<a><slot /></a>'
-VueTestUtils.config.stubs['no-ssr'] = '<span><slot /></span>'
