177a4c73be80b4d5abb505939af16aedd7fe73e5
JHyeok <dev.jhyeok@gmail.com>
2020-05-03 11:42:18 +0900

    feat: Account의 이름 수정 기능 추가

--- components/account/AccountCard.vue
+++ components/account/AccountCard.vue
@@ -7,9 +7,10 @@
         </h5>
       </div>
       <div class="col-4">
-        <div class="info-contents">
+        <div v-if="!modified" class="info-contents">
           {{ user.realName }}
         </div>
+        <b-form-input v-else id="modifiedUserName" v-model="user.realName" />
       </div>
     </div>
     <div class="row justify-content-center">
@@ -37,8 +38,11 @@
       </div>
     </div>
     <div class="row justify-content-end">
-      <button class="btn btn-info">
+      <button v-if="!modified" class="btn btn-info" @click="toggleModified">
         수정
+      </button>
+      <button v-else class="btn btn-success" @click="editUserName(user.id)">
+        저장
       </button>
     </div>
   </div>
@@ -55,6 +59,7 @@ import Component from 'vue-class-component'
 })
 export default class AccountCard extends Vue {
   private modified: boolean = false
+  private isSubmittedToEditName: boolean = false
 
   private toggleModified() {
     this.modified = !this.modified
@@ -62,6 +67,35 @@ export default class AccountCard extends Vue {
 
   private getDate(datetime: Date) {
     return (this as any).$moment(datetime).format('YYYY-MM-DD HH:mm:ss')
+  }
+
+  private async editUserName(userId: string) {
+    try {
+      this.isSubmittedToEditName = true
+
+      const editedUserName = (document as any).getElementById(
+        'modifiedUserName'
+      ).value
+
+      if (editedUserName.length === 0) {
+        ;(this as any).$toast.error('이름을 입력해주세요.')
+        return
+      }
+
+      const res = await (this as any).$axios.put(`/users/${userId}`, {
+        realName: editedUserName,
+      })
+
+      if (res.status === 200) {
+        ;(this as any).user.realName = editedUserName
+        this.modified = false
+        ;(this as any).$toast.success('이름을 수정하였습니다.')
+      }
+    } catch (error) {
+      ;(this as any).$toast.error(error.message as string)
+    } finally {
+      this.isSubmittedToEditName = false
+    }
   }
 }
 </script>
